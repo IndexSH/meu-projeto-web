f987c96e177919d7d139a1f3a3ad6da58ab4fd8c
IndexSH <lucasantonio0987654321@gmail.com>
2026-02-26 13:53:15 -0300

    JavaScript e uns negócios legais

--- meu_projeto_web/JS/script.js
+++ meu_projeto_web/JS/script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+let nada = null;
+console.log(typeof nada);
+console.log(nada);
+
+function nome () {
+    let nome = "Lucas";
+    console.log(nome)
+}
+
+nome()
+nome()
